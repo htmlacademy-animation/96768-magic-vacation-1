5a7307c2a04a738971b5f6b1ff184e42a4887fd2
DitmarGrim <dgavrichkov32@gmail.com>
2021-08-04 23:16:21 +0300

    добавлен оверлей с анимацией

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -16,10 +16,19 @@ export default () => {
   }
 
   for (let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener(`click`, function () {
+    menuLinks[i].addEventListener(`click`, function (e) {
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
+      }
+
+      if (menuLinks[i].dataset.href === `prizes`) {
+        e.preventDefault();
+        const overlay = document.querySelector(`.page-content__overlay`);
+        overlay.classList.add(`is-active`);
+        overlay.addEventListener(`transitionend`, function () {
+          window.location.href = menuLinks[i].href;
+        });
       }
     });
   }
